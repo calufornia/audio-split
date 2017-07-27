34778e115d6b8d0d77387f1b32e5e22aec65f985
Justin Lu <justin@castleglobal.com>
2017-07-27 15:55:41 -0700

    add test

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-var split = require('./index');
+var Promise = require('bluebird');
+var split = Promise.promisify(require('./index'));
+
+const splitClip = Promise.coroutine(function*(filepath) {
+  let paths = yield split({filepath});
+  console.log(paths);
+});
+
+// splitClip('path/to/file.mp4'); // uncomment
