dc3f09adf08f2941c3b081f59b0c03b441b1947a
Justin Lu <justin@castleglobal.com>
2017-08-02 15:30:45 -0700

    change test to audio file

--- test.js
+++ test.js
@@ -2,8 +2,14 @@ var Promise = require('bluebird');
 var split = Promise.promisify(require('./index'));
 
 const splitClip = Promise.coroutine(function*(filepath) {
-  let paths = yield split({filepath});
+  let paths;
+  try {
+    paths = yield split({filepath});
+  } catch (e) {
+    console.log(e);
+    return;
+  }
   console.log(paths);
 });
 
-// splitClip('path/to/file.mp4'); // uncomment
+// splitClip('path/to/file.mp3'); // uncomment
